32a5ce68d13435826badfe2ebed4b96bb8a2a1f0
Zhiminqi <48302264+Zhiminqi@users.noreply.github.com>
2019-08-24 23:39:06 -0700

    Add files via upload

---
+++ component/Main.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import { Profile } from './Profile';
+import nba from 'nba';
+//import ShotChart from './ShotChart';
+import DataViewContainer from './DataViewContainer';
+import SearchBar from './SearchBar';
+import { DEFAULT_PLAYER_INFO } from '../Constants';
+
+export class Main extends React.Component {
+
+    state = {
+        playerInfo: DEFAULT_PLAYER_INFO,
+    }
+
+    componentDidMount() {
+        this.loadPlayerInfo(this.state.playerInfo.fullName);
+    }
+
+    handleSelectPlayer = (playerName) => {
+        this.loadPlayerInfo(playerName);
+    }
+
+    loadPlayerInfo = (playerName) => {
+        nba.stats.playerInfo({ PlayerID: nba.findPlayer(playerName).playerId }).then((info) => {
+            const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            this.setState({ playerInfo });
+        });
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <SearchBar handleSelectPlayer={this.handleSelectPlayer}/>
+                <div className="player">
+                    <Profile playerInfo={this.state.playerInfo}/>
+                    <DataViewContainer playerId={this.state.playerInfo.playerId}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
+
